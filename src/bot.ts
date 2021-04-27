6ea12f134c33e52854af21c097a9e1633059cc91
povilasleka <povilasleka@gmail.com>
2021-04-27 18:18:11 +0300

    Simple bot void program

---
+++ src/bot.ts
@@ -0,0 +1,26 @@
+import {Client, Message} from "discord.js";
+import {inject, injectable} from "inversify";
+import {TYPES} from "./types";
+
+@injectable()
+export class Bot {
+  private client: Client;
+  private readonly token: string;
+
+  constructor(
+    @inject(TYPES.Client) client: Client,
+    @inject(TYPES.Token) token: string
+  ) {
+    this.client = client;
+    this.token = token;
+  }
+
+  public listen(): Promise < string > {
+    this.client.on('message', (message: Message) => {
+      console.log("Message received! Contents: ", message.content);
+      console.log(message.author.username);
+    });
+
+    return this.client.login(this.token);
+  }
+}
